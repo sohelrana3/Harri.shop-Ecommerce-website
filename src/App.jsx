c4dff4b100226c72d0587a04937dbf587996ba38
Sohel Rana <expartdesigns5@gmail.com>
2023-05-31 00:10:33 +0600

    home pages about pages shop pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import react from 'react'
+import React from "react";
+// React Router Dom
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+//import pages
+import RootLayouts from "./components/RootLayouts";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
 
+
+//
 
 function App() {
-  
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<RootLayouts />}>
+                <Route index element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/shop" element={<Shop />}></Route>
+           
+            </Route>
+        )
+    );
 
-  return (
-   
-   <h2>hello</h2>
-  )
+    return <RouterProvider router={router} />;
 }
-
-export default App
+export default App;
